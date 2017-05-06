a58cc1a51f642beff945b184618e4f7a0ed7582e
abdelrahmanElhoshy <wizardooo88888@gmail.com>
2017-05-06 18:51:34 +0200

    signup and login

--- www/js/controllers/UserController.js
+++ www/js/controllers/UserController.js
@@ -1,38 +1,35 @@
 angular.module('ChatApp').controller('user', function($scope, $state, users, $rootScope) {
       $scope.user = {};
-      console.log("user Controller");
-      $scope.inerr = false;
-      $scope.inerrmsg = "message";
-      $scope.userLogin = function() {
+      $scope.myerrors = {};
+ $scope.validetor = {
 
-        if ($scope.user['email'] && $scope.user['password']) {
-
-
-
-            $scope.userCheck = users.checkUserData($scope.user).then(function(res) {
-              console.log(res.status);
-              console.log(res);
-              $scope.dataresult = res.data;
-              if (res.status) {
-                $state.go('app.users');
-              }
-               else {
-                $scope.err = true;
-                $scope.errmsg = "Invalid Email Or password"
-              }
-
-
-            });
-            console.log($scope.userCheck);
-
-          } else {
-            $scope.inerr = true;
-            $scope.inerrmsg = "Invalid Email Or password"
-          }
-        }
+              'passwordLen': function(){
+                if ($scope.user.password && $scope.user.password.length < 4) {
+                    $scope.myerrors.password_len = true;
+                }
+                else {
+                    $scope.myerrors.password_len = false;
+                }
+            }
+        };
+$scope.userLogin = function (valid) {
+  console.log(valid);
+   valid = valid && !$scope.myerrors.password_len && $scope.user.username;
+            if(valid) {
+                $scope.dataresult=users.checkUserData($scope.user);
+                // console.log($scope.userCheck)
+                $rootScope.root_user = $scope.user;
+                // console.log($scop.dataresult);
+                if(false){
+                $state.go('app.users');}
+            } else {
+                console.log($scope.myerrors);
+            }
 
 
-        $scope.myerrors = {};
+}
+
+        
         $scope.validetor = {
           'match': function() {
             console.log($scope.user.password);
