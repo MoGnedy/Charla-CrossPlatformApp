e7fb6768eb5752a821ab59dac41462bd4cc83223
mognedy <mognedy@Gmail.com>
2017-05-16 15:27:03 +0200

    push notification Token

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var serverIp = "127.0.0.1:3000"
+var serverIp = "172.16.2.141:3000"
 var socket = io("http://" + serverIp);
 
 angular.module('ChatApp', ['ionic'])
@@ -30,6 +30,37 @@ angular.module('ChatApp', ['ionic'])
         // from snapping when text inputs are focused. Ionic handles this internally for
         // a much nicer keyboard experience.
         cordova.plugins.Keyboard.disableScroll(true);
+
+        //push notification
+
+        var push = PushNotification.init({
+          android: {
+            senderID: "287432877293"
+          },
+          browser: {
+            pushServiceURL: 'https://api.ionic.io/push/notifications'
+          },
+          windows: {}
+        });
+
+        push.on('registration', function(data) {
+          // data.registrationId
+          console.log(data.registrationId);
+        });
+
+        push.on('notification', function(data) {
+          console.log(data);
+          // data.message,
+          // data.title,
+          // data.count,
+          // data.sound,
+          // data.image,
+          // data.additionalData
+        });
+
+        push.on('error', function(e) {
+          // e.message
+        });
       }
       if (window.StatusBar) {
         StatusBar.styleDefault();
